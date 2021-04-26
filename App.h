aa1b514c87864acf78cd431210e140667ac5cbf1
AxilJ <62996161+AxilJ@users.noreply.github.com>
2021-04-26 14:20:02 +0400

    Added App.h

---
+++ App.h
@@ -0,0 +1,21 @@
+// This is App.h
+// For Application.cpp
+// Make sure to use Libraries that are C++
+// Or errors will appear
+/**/
+
+#pragma once
+
+// These are all the includes
+// To use for Ayzern Engine
+/**/
+
+#include <AccCtrl.h>
+#include <AclAPI.h>
+#include <activation.h>
+#include <activdbg.h>
+#include <activdbg100.h>
+#include <iostream>
+#include <activecf.h>
+#include <ActiveDS.h>
+#include <activprof.h>
