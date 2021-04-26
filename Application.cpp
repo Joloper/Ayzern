47b583d537fd86d5311c348adda63e433c784fa2
AxilJ <62996161+AxilJ@users.noreply.github.com>
2021-04-26 14:21:03 +0400

    Added Application.cpp

---
+++ Application.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright info
+// Free to use Ayzern
+// Make sure to use Libraries that are C++
+// Or errors will appear
+/**/
+
+#include <iostream>
+#include "App.h"
+
+class Copyright {
+
+	std::nullptr_t
+	CPP_AYZERN() = NULL;
+
+};
+
+class Ayzern {
+
+	// This is free to use
+	// Asset Store can include free assets
+	/**/
+
+	bool Ayzern;
+
+	if (Ayzern) {
+		return true;
+	}
+
+};
+
+_STD_END;
+#endif;
