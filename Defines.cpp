d066c8e61cb27eefcc578ffab30730489bc48ca9
AxilJ <62996161+AxilJ@users.noreply.github.com>
2021-04-27 16:29:07 +0400

    Added // Optional Words

--- Defines.cpp
+++ Defines.cpp
@@ -1,3 +1,10 @@
+// This is Defines CPP File for defines in Ayzern
+/**/
+
+#include <iostream>
 #include "Defines.h"
+
+// Here are all the defines that are in use
+// Be sure they are correct
 
 #define
