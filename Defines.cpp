fd142322bdb17c597fccb96f57a20061d3b68892
AxilJ <62996161+AxilJ@users.noreply.github.com>
2021-04-27 20:49:08 +0400

    Added more Defines to Define.cpp (YepZir Helped)

--- Defines.cpp
+++ Defines.cpp
@@ -7,4 +7,8 @@
 // Here are all the defines that are in use
 // Be sure they are correct
 
-#define
+#define AN_REPEAT = NULL;
+#define AN_NULL_AYZERN_DEBUG;
+#define bool NULL;
+#define AN.Ayzern_Debug;
+#define AN.RepeatDebug;
