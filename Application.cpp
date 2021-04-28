435f861e31dc37057151a74a87ddd7ff87754063
Programzer <83328710+Programzer@users.noreply.github.com>
2021-04-28 13:33:34 +0400

    Added a Triangle in Application.cpp

--- Application.cpp
+++ Application.cpp
@@ -48,8 +48,13 @@ class STD {
        virtual bool WindowSTD	       
        std::print(KazdSTD) == true;
        std::print(WindowSTD) == false;	
-	
-}	
+
+     float vertices[] = {
+     0.0f,  0.5f, // Vertex 1 (X, Y)
+     0.5f, -0.5f, // Vertex 2 (X, Y)
+    -0.5f, -0.5f  // Vertex 3 (X, Y)
+};
+}
 
 _STD_END;
 #endif;
