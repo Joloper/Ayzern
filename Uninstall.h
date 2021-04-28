498a24a2b261469cf1aa56a07708d62c36c04762
YepZir <83290750+YepZir@users.noreply.github.com>
2021-04-28 16:06:11 +0400

    Added Uninstall Debug/ImGuiLayer Remove

--- Uninstall.h
+++ Uninstall.h
@@ -1,1 +1,22 @@
+#pragma once
 
+namespace Hazel {
+
+	class Layer
+	{
+	public:
+		Layer(const std::string& name = "Layer");
+		virtual ~Layer() = default;
+
+		virtual void OnAttach() {}
+		virtual void OnDetach() {}
+		virtual void OnUpdate(Timestep ts) {}
+		virtual void OnImGuiRender() {}
+		virtual void OnEvent(Event& event) {}
+
+		const std::string& GetName() const { return m_DebugName; }
+	protected:
+		std::string m_DebugName;
+	};
+
+}
