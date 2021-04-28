890745b41e1503510e7ab33aec00dc09ba528514
AxilJ <62996161+AxilJ@users.noreply.github.com>
2021-04-28 14:44:45 +0400

    Added premake5.lua for Ayzern

---
+++ premake5.lua
@@ -0,0 +1,27 @@
+include "./vendor/premake/premake_customization/solution_items.lua"
+
+workspace "Ayzern"
+	architecture "x86_64"
+	startproject "Ayzern Editor"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	solution_items
+	{
+		".editorconfig"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
