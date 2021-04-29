c8ada24427c222d37dd081762636bb7c9297e37f
AxilJ <62996161+AxilJ@users.noreply.github.com>
2021-04-29 12:47:41 +0400

    Added Input.h

---
+++ AyzernFolder/Input.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+
+namespace Ayzern {
+
+	class Input
+	{
+	public:
+		static bool IsKeyPressed(KeyCode key);
+
+		static bool IsMouseButtonPressed(MouseCode button);
+		static glm::vec2 GetMousePosition();
+		static float GetMouseX();
+		static float GetMouseY();
+	};
+}
