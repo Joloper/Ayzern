d763f9dc6e46790353b868d76c635c2c5ee95e02
Programzer <83328710+Programzer@users.noreply.github.com>
2021-04-29 12:56:04 +0400

    Added Layer.h Plus DebugName Detect

---
+++ AyzernFolder/Layer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+namespace Ayzern {
+
+	class Layer
+	{
+	public:
+		Layer(const std::string& name = "Layer");
+		virtual ~Layer() = default;
+
+		virtual void OnAttach() {}
+		virtual void OnDetach() {}
+		virtual void OnUpdate(Timestep ts) {}
+		virtual void OnImGuiRender() {}
+		virtual void OnEvent(Event& event) {}
+
+		const std::string& GetName() const { return m_DebugName; }
+	protected:
+		std::string m_DebugName;
+	};
+
+}
