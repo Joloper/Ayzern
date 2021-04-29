df9a07bcb7a26b4d3e9ce6ca8bff606d8a55f571
AxilJ <62996161+AxilJ@users.noreply.github.com>
2021-04-29 10:47:11 +0400

    Added Defs for Ayzern.h

--- AyzernFolder/Ayzern.h
+++ AyzernFolder/Ayzern.h
@@ -1,1 +1,29 @@
+#pragma once
+
+#include "Ayzern"
+
+#ifdef AZ_PLATFORM_WINDOWS
+#ifndef NOMINMAX
+// See github.com/skypjack/entt/wiki/Frequently-Asked-Questions#warning-c4003-the-min-the-max-and-the-macro
+#define NOMINMAX
+#endif
+#endif
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <sstream>
+#include <array>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+
+#ifdef AZ_PLATFORM_WINDOWS
+#include <Windows.h>
+#endif
 
