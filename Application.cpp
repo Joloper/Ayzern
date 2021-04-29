f396eab0189f3d853cf0f916b6a7225695d99eeb
YepZir <83290750+YepZir@users.noreply.github.com>
2021-04-29 20:33:31 +0400

    Added Window Resize/Profile Function

--- Application.cpp
+++ Application.cpp
@@ -56,6 +56,86 @@ class STD {
 };
 }
 
+void Application::Close()
+{
+	m_Running = false;
+}
+
+void Application::OnEvent(Event& e)
+{
+	AZ_PROFILE_FUNCTION();
+
+	EventDispatcher dispatcher(e);
+	dispatcher.Dispatch<WindowCloseEvent>(HZ_BIND_EVENT_FN(Application::OnWindowClose));
+	dispatcher.Dispatch<WindowResizeEvent>(HZ_BIND_EVENT_FN(Application::OnWindowResize));
+
+	for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); ++it)
+	{
+		if (e.Handled)
+			break;
+		(*it)->OnEvent(e);
+	}
+}
+
+void Application::Run()
+{
+	AZ_PROFILE_FUNCTION();
+
+	while (m_Running)
+	{
+		AZ_PROFILE_SCOPE("RunLoop");
+
+		float time = (float)glfwGetTime();
+		Timestep timestep = time - m_LastFrameTime;
+		m_LastFrameTime = time;
+
+		if (!m_Minimized)
+		{
+			{
+				AZ_PROFILE_SCOPE("LayerStack OnUpdate");
+
+				for (Layer* layer : m_LayerStack)
+					layer->OnUpdate(timestep);
+			}
+
+			m_ImGuiLayer->Begin();
+			{
+				AZ_PROFILE_SCOPE("LayerStack OnImGuiRender");
+
+				for (Layer* layer : m_LayerStack)
+					layer->OnImGuiRender();
+			}
+			m_ImGuiLayer->End();
+		}
+
+		m_Window->OnUpdate();
+	}
+}
+
+bool Application::OnWindowClose(WindowCloseEvent& e)
+{
+	m_Running = false;
+	return true;
+}
+
+bool Application::OnWindowResize(WindowResizeEvent& e)
+{
+	AZ_PROFILE_FUNCTION();
+
+	if (e.GetWidth() == 0 || e.GetHeight() == 0)
+	{
+		m_Minimized = true;
+		return false;
+	}
+
+	m_Minimized = false;
+	Renderer::OnWindowResize(e.GetWidth(), e.GetHeight());
+
+	return false;
+}
+
+}
+
 class End {
 
 	_STD_END;
