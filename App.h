3538bbf97ae919e4d5eb91ae14b83c15efde122b
Programzer <83328710+Programzer@users.noreply.github.com>
2021-04-29 13:08:01 +0400

    Added ImGuiLayer +Friend/Float

--- App.h
+++ App.h
@@ -22,3 +22,42 @@
 #include <activprof.h>
 #include "App.h"
 #include "Defines.h"
+
+class Application
+{
+public:
+	Application(const std::string& name = "Ayzern App");
+	virtual ~Application();
+
+	void OnEvent(Event& e);
+
+	void PushLayer(Layer* layer);
+	void PushOverlay(Layer* layer);
+
+	Window& GetWindow() { return *m_Window; }
+
+	void Close();
+
+	ImGuiLayer* GetImGuiLayer() { return m_ImGuiLayer; }
+
+	static Application& Get() { return *s_Instance; }
+private:
+	void Run();
+	bool OnWindowClose(WindowCloseEvent& e);
+	bool OnWindowResize(WindowResizeEvent& e);
+private:
+	Scope<Window> m_Window;
+	ImGuiLayer* m_ImGuiLayer;
+	bool m_Running = true;
+	bool m_Minimized = false;
+	LayerStack m_LayerStack;
+	float m_LastFrameTime = 0.0f;
+private:
+	static Application* s_Instance;
+	friend int ::main(int argc, char** argv);
+};
+
+// To be defined in CLIENT
+Application* CreateApplication();
+
+}
